cb80310998af775fffbf359c6a4636cf2081f12a
GreenMoonMan <GreenMoonMan@users.noreply.github.com>
2026-04-23 05:23:00 +0000

    Revert "updated start uart to not use fancy bash prompt"
    This reverts commit f69e5ce14f1890d487bcedcd43a33bee1de0ae3b.

--- .devcontainer/scripts/start_uart_sim.sh
+++ .devcontainer/scripts/start_uart_sim.sh
@@ -5,18 +5,18 @@ PIDFILE=/tmp/uart-sim.pid
 PTY=/tmp/ttyUART0
 
 # Kill any existing instance
-if [ -f "$PIDFILE" ] && kill -0 "$(cat "$PIDFILE")" 2>/dev/null; then
-    echo "[uart-sim] Already running (PID $(cat "$PIDFILE")), skipping."
+if [ -f "$PIDFILE" ] && kill -0 "$(cat $PIDFILE)" 2>/dev/null; then
+    echo "[uart-sim] Already running (PID $(cat $PIDFILE)), skipping."
     exit 0
 fi
 
 # Clean up stale symlink from a previous container run
 rm -f "$PTY"
 
-# Start socat: PTY on one end, clean bash shell on the other
+# Start socat: PTY on one end, bash shell on the other
 socat \
-  PTY,link="${PTY}",raw,echo=0 \
-  EXEC:'env -i HOME=/root PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin TERM=vt100 PS1="\u@\h:\w\\$ " bash --noprofile --norc -i',pty,setsid,stderr,sigint,sane \
+  PTY,link=${PTY},raw,echo=0 \
+  EXEC:bash,pty,setsid,stderr,sigint,sane \
   &
 
 echo $! > "$PIDFILE"
